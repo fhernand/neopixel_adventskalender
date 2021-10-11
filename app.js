ffb80b8315f8473d21432de12d7ff505a72e89ec
fhernand <feli.hp@gmail.com>
2021-10-11 13:27:02 +0200

    minor bugfix

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ class Main {
         var now = new Date;
 
         this.ledGroups = [
-          new ledGroup([0,3,4,7,8,11,12,15,16,19,20,23,24,27,28,31,32,35,36,39,40,43,44,47,48],now, 'seconds', {"on": "0xBEFF33", "before": "0x5A0AAB", "after":"0xE85D13"}),
-          new ledGroup([4,11,18],now, 'seconds', {"on": "0x10FF33", "before": "0x100AAB", "after":"0x105D13"}),
-          new ledGroup([1,2,5,6,9,10,13,14,17,18,21,22,25,26,29,30,33,34,37,38,41,42,45,46,49],0, 'timeless', {"on": "0xFFFFFF"})
+          new ledGroup("days",[0,3,4,7,8,11,12,15,16,19,20,23,24,27,28,31,32,35,36,39,40,43,44,47,48],now, 1, 'seconds', {"on": "0xBEFF33", "before": "0x5A0AAB", "after":"0xE85D13"}),
+          new ledGroup("advent",[4,11,18],now, 7, 'seconds', {"on": "0x10FF33", "before": "0x100AAB", "after":"0x105D13"}),
+          new ledGroup("hintergrund",[1,2,5,6,9,10,13,14,17,18,21,22,25,26,29,30,33,34,37,38,41,42,45,46,49],0,0, 'timeless', {"on": "0xFFFFFF"})
         ];
 
         this.ledGroups.forEach (ledGroup => {
@@ -77,9 +77,11 @@ class Main {
 };
 
 class ledGroup {
-  constructor(ledArray, startTime, validityType, colors){
+  constructor(name, ledArray, startTime, delta, validityType, colors){
+    this.name = name;
     this.startTime = new Date(startTime);
     this.leds = ledArray;
+    this.delta = delta;
     this.validityType = validityType;
     this.colorOn = colors.on;
     this.colorBefore = colors.before;
@@ -100,19 +102,19 @@ class ledGroup {
     switch(this.validityType) {
       case 'date':
         var now = nowDate.getDate();
-        var validity = validityTemp.setDate(this.startTime.getDate() + index);
+        var validity = validityTemp.setDate(this.startTime.getDate() + index*this.delta);
         break;
       case 'hours':
         var now = nowDate.getHours();
-        validity = validityTemp.getHours(this.startTime.getHours() + index);
+        validity = validityTemp.getHours(this.startTime.getHours() + index*this.delta);
         break;
       case 'minutes':
         var now = nowDate.getMinutes();
-        validity = validityTemp.setMinutes(this.startTime.getMinutes() + index);
+        validity = validityTemp.setMinutes(this.startTime.getMinutes() + index*this.delta);
         break;
       case 'seconds':
         var now = nowDate.getSeconds();
-        validity = validityTemp.setSeconds(this.startTime.getSeconds() + index);
+        validity = validityTemp.setSeconds(this.startTime.getSeconds() + index*this.delta);
         break;
     }
 
@@ -128,7 +130,7 @@ class ledGroup {
 
   getLedColor(led){
     var index = this.leds.findIndex(element => element == led);
-    
+    console.log(this.name + ":" led + "->" + index);
     if (index!=undefined){
       var state = this.getState(index);
       switch(state){
