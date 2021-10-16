2993e4ae3ad86ecbffb08debc3abcd0fd21af581
fhernand <feli.hp@gmail.com>
2021-10-16 16:50:21 +0200

    Config file support

--- app.js
+++ app.js
@@ -168,6 +168,7 @@ class Config {
     }else{
       this.configFile = 'config.json';
     }
+    console.log(args);
     console.log(this.configFile);
     this.config = jsonfile.readFileSync(this.configFile);
   }
