58e70f1f98576b939e42c3ce0e365445db63b732
fhernand <feli.hp@gmail.com>
2021-10-16 19:05:21 +0200

    Config file support

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ class ledGroup {
   var b = ((num >> 8) & 0x00FF) + amt;
   var g = (num & 0x0000FF) + amt;
   var newColor = g | (b << 8) | (r << 16);
-  newColor = "0x" + newColor;
+  newColor = '#' + newColor;
   return newColor.toString(16);
 }
 }
