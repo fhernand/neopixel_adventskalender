3088704772246db2c93e806684525d8a7891776e
fhernand <feli.hp@gmail.com>
2021-10-17 15:10:18 +0200

    Config file support

---
+++ clear.js
@@ -0,0 +1,40 @@
+const ws281x = require('rpi-ws281x');
+
+class Main {
+
+    constructor() {
+        var config = new Config();
+        // Current pixel position
+        this.pixel = 0;
+
+        this.config = {};
+
+        // Number of leds in my strip
+        this.config.leds = 50;
+
+        // Use DMA 10 (default 10)
+        this.config.dma = 10;
+
+        // Set full brightness, a value from 0 to 255 (default 255)
+        this.config.brightness = 0;
+
+        // Set the GPIO number to communicate with the Neopixel strip (default 18)
+        this.config.gpio = 18;
+
+        // The RGB sequence may vary on some strips. Valid values
+        // are "rgb", "rbg", "grb", "gbr", "bgr", "brg".
+        // Default is "rgb".
+        // RGBW strips are not currently supported.
+        this.config.stripType = 'grb';
+
+        // Configure ws281x
+        ws281x.configure(this.config);
+
+        this.pixels = new Uint32Array(this.config.leds);
+
+        ws281x.render(this.pixels);
+    }
+
+};
+
+var main = new Main();
